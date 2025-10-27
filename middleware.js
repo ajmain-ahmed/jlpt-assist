2ee84ca1d7b79fe4ff7eb213cf15cbcefca88226
Noon <gdaftrnoon@gmail.com>
2025-10-27 20:58:40 +0000

    ??

--- middleware.js
+++ middleware.js
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 import { auth } from "./app/auth";
+import { redirect } from "next/navigation";
 
 export default async function middlware(request) {
     const sessionObj = await auth()
     const userid = sessionObj?.user?.userId
 
     if (!userid) {
-        return NextResponse.redirect(
-            new URL('/', request.url)
-        )
+        redirect('/')
     }
     return NextResponse.next()
 }
